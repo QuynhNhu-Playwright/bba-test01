65663229aff1df7664966b4642c435fba46a9d79
test 01 - Như <aquarius3294@gmail.com>
2025-12-07 21:33:31 +0700

    feat: add solution for challenge 03

--- tests/lesson-04/cloud-kingdom.js
+++ tests/lesson-04/cloud-kingdom.js
@@ -1,14 +1,14 @@
 const playerName = "Mario";
 const currentLives = 3;
 const coins = [
-    {Level: 1, coin: 25},
-    {Level: 2, coin: 30},
-    {Level: 3, coin: 45},
+    { Level: 1, coin: 25 },
+    { Level: 2, coin: 30 },
+    { Level: 3, coin: 45 },
 ];
 let sum = 0;
 let avg = 0
-for (let s = 0; s < coins.length; s++){
-     sum = sum + coins[s].coin
-     avg = sum % 3;
+for (let s = 0; s < coins.length; s++) {
+    sum = sum + coins[s].coin
+    avg = sum % 3;
 };
 console.log("số coin dư:", avg);
